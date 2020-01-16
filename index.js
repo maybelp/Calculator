2c8e50d1f11f3b16156e444af1685d7c8b10a11c
First Last <maybelp2006@gmail.com>
2020-01-16 22:42:54 +0000

    switch

--- index.js
+++ index.js
@@ -1,16 +1,66 @@
-//Branching
-const readline = require('readline-sync');
+//Branching code
+const readline = require('readline-sync'); 
+
+function wrongOne(){
+    throw new Error('InvalidOperatorNo');
+
+}
+function getOperator(oprtr) {
+    
+    let voperator = ['+', '-', '*', '/'];
+    let postVoperator = 0;
+    let found = false;
+    while (postVoperator < voperator.length){
+        if (voperator[postVoperator] != oprtr) { ++postVoperator; }
+        else{
+            postVoperator = voperator.length;
+            found = true;
+        }    
+    }
+    if (found == true) {
+      return oprtr;
+    } else {
+      wrongOne(); // throw keyword is used here
+    }
+}
+function init(){
+    try { // statements to try
+        operatorName = getOperator(myOperator); // function could throw exception
+    }
+    catch (e) {
+        operatorName = 'unknown';
+        console.error(e);
+        //logMyErrors(e); // pass exception object to error handler (i.e. your own function)
+    }
+
 console.log('Welcome to the Calculator \n');
 console.log('==========================\n');
-console.log('please enter the operator: ')
+console.log('please enter the operator: ');
 operator = readline.prompt();
-console.log('Please enter the first number:');
-number1 = +readline.prompt();
-console.log('Please enter the second number:');
-number2 = +readline.prompt();
-if (operator == '+') { result = number1 + number2; }
-else if (operator == '-') { result = number1 - number2; }
-else if (operator == '*') { result = number1 * number2; }
-else { result = (operator == '/')?number1 / number2:'Error'; }
-console.log(result);
-console.log(' %d %s %d = %d', number1, operator, number2, result);
+operator = getOperator(operator); 
+console.log('please enter the first number: ');
+fNumber = readline.prompt();
+console.log('Please enter the second number: ');
+sNumber = readline.prompt();
+switch (operator) {
+    case '+':
+        result = fNumber + sNumber;
+      break;
+    case '-':
+        result = fNumber - sNumber;
+      break;
+    case '*': 
+        result = fNumber * sNumber;
+        
+      break;
+    case '/':
+        if (sNumber != 0) {result = fNumber / sNumber;}
+        else{console.log (Error)}
+      break;
+    default:
+     console.log(`Sorry, ${operator} NaN.`);
+  }
+
+console.log(' The answer is: %d', result);
+}
+init();
